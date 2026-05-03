07a572b4a4ed74a33877ef780f5733c085735529
Adam Byrne <adamrbyrne@gmail.com>
2026-05-03 12:24:13 +0100

    chore: compiles to bootable image, runnable via qemu

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@
 
 use core::panic::PanicInfo;
 
+const VGA_BUFFER: *mut u8 = 0xb8000 as *mut u8;
+
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
@@ -10,7 +12,14 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    // Your code here
+    let message = b"Hello, World!";
+    for (i, &byte) in message.iter().enumerate() {
+        unsafe {
+            *VGA_BUFFER.offset(i as isize * 2) = byte; // Write character
+            *VGA_BUFFER.offset(i as isize * 2 + 1) = 0x07; // Set color (white on black)
+        }
+    }
+
     loop {}
 }
 

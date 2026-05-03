5748eb05358241a05cc1beeac64d8659011a59d4
Adam Byrne <adamrbyrne@gmail.com>
2026-05-03 13:09:08 +0100

    test: basic custom test runner

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 mod vga_buffer;
 
@@ -15,7 +18,24 @@ fn panic(info: &PanicInfo) -> ! {
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
         println!("Hello World{}", "!");
+    #[cfg(test)]
+    test_main();
 
     loop {}
 }
 
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
+    assert_eq!(1, 1);
+}
